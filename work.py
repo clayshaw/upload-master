22592a0673993db7addc121f79d3c727cf79fb3d
clayshaw <89714586+clayshaw@users.noreply.github.com>
2023-02-12 00:14:58 +0800

    Update work.py

--- work.py
+++ work.py
@@ -1,13 +1,17 @@
 import openpyxl
 from openpyxl.styles import Font 
 import os
-import difflib
 import math
 from tkinter import *
 import tkinter.messagebox
 
 
 def cmpSame(s1,s2):
+    # a = difflib.SequenceMatcher(None, s1, s2).quick_ratio()
+    # if(a>0.85):
+    #     return True
+    # else:
+    #     return False
     if(len(s1)==len(s2)):
         for i in range(0,len(s1)-1):
             if(s1[i]!=s2[i]):
@@ -33,10 +37,17 @@ def mainfunc(s):
 
     for rowStr in range(2,sheet.max_row,1):
         valName.append(sheet.cell(rowStr, 1 ).value)
+        # print(sheet.cell(rowStr, 1 ).value)
     for rowNum in range(2,sheet.max_row):
         valNum.append(sheet.cell(rowNum, 2).value)
+        # print(sheet.cell(rowNum, 2).value)
     for i in range(0, len(valName)):
         cmp[valName[i]] = valNum[i]
+    # for i in range(0, len(valName)):
+    #     print(cmp[valName[i]]) 
+
+    valName.append('')
+    valNum.append(0)
 
     cnt = 2
     iscon = False
@@ -55,6 +66,7 @@ def mainfunc(s):
             sheet.cell(cnt,7).value = valNum[i]
         cnt+=1
         
+    # wb.save(r'C:\Users\user\Desktop\workspace\python\excel\data1.xlsx')
     wb.save(s)
     print("Done")
 
@@ -64,7 +76,7 @@ def mainfunc(s):
 
 #圖形化介面處理
 
-def exception(val):
+def inspect(val):
     filena, extension = os.path.splitext(val)
     if(os.path.exists(val) and extension == '.xlsx'):
         mainfunc(val)
@@ -88,9 +100,24 @@ lab2.place(relx=0.3,rely=0.2)
 inp1 = Entry(root)
 inp1.place(relx=0.15,rely=0.4,relwidth=0.7,relheight=0.1)
 
-btn = Button(root,text='執行',command=lambda: exception(inp1.get()))
+btn = Button(root,text='執行',command=lambda: inspect(inp1.get()))
 btn.place(relx = 0.4,rely=0.6,relwidth=0.2,relheight=0.1)
 
 root.mainloop()
 
 #--------------------------------------
+
+# while(1):
+#     print("請輸入excel檔案位置(絕對路徑)   輸入E結束程式:"+'\n'+"範例:  C:\\Users\\excel\\data.xlsx")
+#     val = input()
+#     if(val=='E'):
+#         break
+#     if(val == "cls"):
+#         os.system("cls")
+#         continue
+#     root, extension = os.path.splitext(val)
+#     if(os.path.exists(val) and extension == '.xlsx'):
+#         mainfunc(val)
+#     else:
+#         print(val+"並非正確的檔案位置，請輸入正確檔案位置及檔名")
+#     print("")
